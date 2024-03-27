39236d48c0fe3eff5032da027a222ade0e317460
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2024-03-27 02:00:32 +0000

    Make Loki scalable

--- create-containers.sh
+++ create-containers.sh
@@ -1,3 +1,7 @@
-az storage container create -n tempo --connection-string "DefaultEndpointsProtocol=http;AccountName={{ env "AZURE_STORAGE_ACCOUNT" }};AccountKey={{ env "AZURE_STORAGE_KEY" }};BlobEndpoint={{ env "AZURE_ENDPOINT" }};"
-az storage container create -n loki --connection-string "DefaultEndpointsProtocol=http;AccountName={{ env "AZURE_STORAGE_ACCOUNT" }};AccountKey={{ env "AZURE_STORAGE_KEY" }};BlobEndpoint={{ env "AZURE_ENDPOINT" }};"
-az storage container create -n loki-ruler --connection-string "DefaultEndpointsProtocol=http;AccountName={{ env "AZURE_STORAGE_ACCOUNT" }};AccountKey={{ env "AZURE_STORAGE_KEY" }};BlobEndpoint={{ env "AZURE_ENDPOINT" }};"
+CONN_STRING="DefaultEndpointsProtocol=http;AccountName={{ env "AZURE_STORAGE_ACCOUNT" }};AccountKey={{ env "AZURE_STORAGE_KEY" }};BlobEndpoint={{ env "AZURE_ENDPOINT" }};"
+
+echo $CONN_STRING
+
+az storage container create -n tempo --connection-string $CONN_STRING
+az storage container create -n loki --connection-string $CONN_STRING
+az storage container create -n loki-ruler --connection-string $CONN_STRING
